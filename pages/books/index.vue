8e639b17f0a3243ac368fd41f54a335ed43f106a
dev-kozaka <kozahooo443@yahoo.co.jp>
2023-09-27 17:12:27 +0900

    Add: axios

---
+++ pages/books/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>こんにちは</h1>
+    <div>{{hogehoge}}</div>
+    <div>{{items}}</div>
+  </div>
+</template>
+
+<script>
+import $axios from 'axios';
+
+export default {
+  data() {
+    return {
+      hogehoge: "適当",
+      items: []
+    }
+  },
+  mounted() {
+    this.hoge()
+  },
+  methods: {
+    async hoge() {
+      const url = "http://localhost:3000/books";
+      const response = await $axios.get(url)
+      this.items.push(response)
+    }
+  }
+}
+</script>
